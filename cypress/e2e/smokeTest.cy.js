bf9d6572f3ad6ed9ea929acdfef766c3d7ba9918
Maquet Dorian <takenzmr@gmail.com>
2024-01-31 11:31:28 +0100

    SmokeTest Effectué

--- cypress/e2e/smokeTest.cy.js
+++ cypress/e2e/smokeTest.cy.js
@@ -8,6 +8,7 @@ context("Vérification des champs de connexion et du bouton", () => {
     });
 
         describe("Verification des champs de connexion et du bouton", () => {
+
             it("Verification des champs de connexion", () => {
 
             cy.get('[data-cy="login-input-username"]').should('exist');
@@ -18,8 +19,11 @@ context("Vérification des champs de connexion et du bouton", () => {
 });
 
 context("Vérification boutons ajout au panier", () => {
+
     describe("Conexion au site", () => {
+
         it('Connexion du client', () => {
+
             cy.visit('http://localhost:8080');
             cy.get('[data-cy=nav-link-login]').click();
             cy.get('[data-cy=login-input-username]')
@@ -29,12 +33,25 @@ context("Vérification boutons ajout au panier", () => {
             .type(login.password)
             .should("have.value", login.password);
             cy.get('[data-cy=login-submit]').click();
-        });
-
-        it("Vérification du bouton ajout sur le produit", () => {
+            cy.get('[data-cy=nav-link-logout]').should('exist');
             cy.visit('http://localhost:8080/#/products');
             cy.get('[data-cy=product-link]').first().click();
             cy.get('[data-cy=detail-product-add]').should('exist');
-        })
-      });
-})
+        });
+    });
+});
+
+context("Vérification de la présence du champ de disponibilité du produit", () => {
+
+    describe("Vérification du champs", () => {
+
+        it('Vers les produits', () => {
+
+            cy.visit('http://localhost:8080/#/products');
+            cy.get('[data-cy="product-link"]').first().click();
+            cy.get('[data-cy="detail-product-stock"]').should('exist');  
+        });
+        
+    });
+});
+
